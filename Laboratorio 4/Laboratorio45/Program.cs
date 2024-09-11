60441a11656c8fff9e7100856a3fea119321a2d7
Gabriel Rodriguez <gabriel-04r@hotmail.com>
2024-09-10 19:50:29 -0500

    Laboratorio 4-5

---
+++ Laboratorio 4/Laboratorio45/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Declaración de variables
+
+        int edad1;
+        int edad2;
+        int edad3;
+        int edad4;
+        int edad5 = 31; 
+
+        // Inicialización de variables
+        edad1 = edad2 = edad3 = edad4 = 32;
+
+        // Corregida la cantidad de argumentos en Console.WriteLine
+        Console.WriteLine("{0}, {1}, {2}, {3}, {4}", edad1, edad2, edad3, edad4, edad5);
+    }
+}
