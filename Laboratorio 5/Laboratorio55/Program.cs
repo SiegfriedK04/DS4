ce2febfd479428a0481439520e4fb8d5fd66a13f
Gabriel Rodriguez <gabriel-04r@hotmail.com>
2024-09-11 18:59:45 -0500

    Laboratorio 5-5

---
+++ Laboratorio 5/Laboratorio55/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Estudiante
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+
+    
+    public void Imprimir_Estudiantes()
+    {
+        List<Estudiante> estudiantes = new List<Estudiante>
+        {
+            new Estudiante { Nombre = "Ana", Edad = 12 },
+            new Estudiante { Nombre = "Juan", Edad = 10 },
+            new Estudiante { Nombre = "Sofia", Edad = 11 }
+        };
+
+        foreach (Estudiante estudiante in estudiantes)
+        {
+            
+            Console.WriteLine("Nombre: " + estudiante.Nombre + ", Edad: " + estudiante.Edad);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estudiante estudiante = new Estudiante();
+        estudiante.Imprimir_Estudiantes(); 
+    }
+}
