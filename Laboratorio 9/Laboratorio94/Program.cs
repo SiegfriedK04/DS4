c99af33233b315b707e7514214a0bf8af124651f
Gabriel Rodriguez <gabriel-04r@hotmail.com>
2024-09-24 20:53:18 -0500

    Laboratorio 9 Completo

---
+++ Laboratorio 9/Laboratorio94/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Aleatorios
+{
+    private Random random = new Random();
+
+    public int Gen_Numero(int min, int max)
+    {
+        return random.Next(min, max);
+    }
+
+    public int[] Gen_Arreglo(int min, int max, int cantidad)
+    {
+        int[] arreglo = new int[cantidad];
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = random.Next(min, max);
+        }
+        return arreglo;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Aleatorios aleatorio = new Aleatorios();
+
+        int Numero_Random = aleatorio.Gen_Numero(1, 100);
+        Console.WriteLine($"Numero aleatorio entre 1 y 100: {Numero_Random}");
+
+        int[] Arreglo_Random = aleatorio.Gen_Arreglo(1, 50, 5);
+        Console.WriteLine("Arreglo de numeros aleatorios entre 1 y 50:");
+        foreach (int numero in Arreglo_Random)
+        {
+            Console.WriteLine(numero);
+        }
+        Console.ReadKey();
+    }
+}
---
+++ Laboratorio 9/Laboratorio95/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Aleatorios
+{
+    private Random random = new Random();
+
+    public int[] ArregloNoRepetido(int min, int max, int cantidad)
+    {
+        HashSet<int> numeros = new HashSet<int>();
+        while (numeros.Count < cantidad)
+        {
+            numeros.Add(random.Next(min, max));
+        }
+        return new List<int>(numeros).ToArray();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Aleatorios aleatorio = new Aleatorios();
+
+        int[] NoRepetir = aleatorio.ArregloNoRepetido(1, 100, 10);
+
+        Console.WriteLine("Numeros generados:");
+        foreach (int numero in NoRepetir)
+        {
+            Console.WriteLine(numero);
+        }
+        Console.ReadKey();
+    }
+}
